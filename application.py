a7a38ee3a6053eec60d79df516a1978a4de54ec2
五十嵐 博司 <h-igarashi@sn.local>
2019-01-29 17:41:05 +0900

    save

--- application.py
+++ application.py
@@ -5,7 +5,10 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from models import *
-import datetime, sys
+import datetime, sys, re
+from jinja2 import evalcontextfilter, Markup, escape
+
+_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -28,6 +31,15 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
+
+@app.template_filter()
+@evalcontextfilter
+def nl2br(eval_ctx, value):
+    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
+        for p in _paragraph_re.split(escape(value)))
+    if eval_ctx.autoescape:
+        result = Markup(result)
+    return result
 
 def checkPassword( password, confiromPassword, username ):
     if( len(password) <7 ):
