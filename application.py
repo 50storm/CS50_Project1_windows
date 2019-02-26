9b4ba1c6aca6e16ca3f298711ea02ded808f0368
Igarashi <aaaaaaa@aaaa.com>
2019-02-26 11:13:45 +0900

    modified initialization and added log function

--- application.py
+++ application.py
@@ -5,9 +5,29 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from jinja2 import evalcontextfilter, Markup, escape
-from flask_sqlalchemy import SQLAlchemy 
-# from logging.handlers import RotatingFileHandler
+from logging.config import dictConfig
 
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'file': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'formatter': 'file',
+            'filename': './log/bookrevie_app.log',
+            'backupCount': 3,
+            'when': 'D',
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['file']
+    }
+})
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -19,14 +39,6 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-
-#Add Igarashi
-app.config["SQLALCHEMY_DATABASE_URI"]  = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-
-#Create SQLAlchemy Instance
-db = SQLAlchemy()
-db.init_app(app)
 Session(app)
 
 # Set up database
--- import.py
+++ import.py
@@ -4,8 +4,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-#conn = "postgres://hiroshi:Tera54hiro@localhost/bookreview"
-conn = "postgres://clbljlcvobhpyp:56202fd3f8b91471a55b86a150995293b195305c2a2cbf5ec0fc1e0314e1022c@ec2-107-22-221-60.compute-1.amazonaws.com:5432/ddrle3028kagbu"
+conn = "postgres://hiroshi:Tera54hiro@localhost/bookreview"
+#conn = "postgres://clbljlcvobhpyp:56202fd3f8b91471a55b86a150995293b195305c2a2cbf5ec0fc1e0314e1022c@ec2-107-22-221-60.compute-1.amazonaws.com:5432/ddrle3028kagbu"
 
 engin = create_engine(conn)
 db = scoped_session(sessionmaker(bind=engin))
@@ -13,7 +13,7 @@ db = scoped_session(sessionmaker(bind=engin))
 def main():
     f = open("books.csv")
     reader = csv.reader(f)
-    i=0;
+    i=0
     for isbn, title, author, year in reader:
         if (i!=0) :
             print(isbn)
@@ -32,7 +32,8 @@ def main():
         else:
             i=i+1
 
-    print(str(i-1) + " was instered!")
+    print(str(i-1) + " books was instered!")
+    db.close()
 
 
 if __name__ == "__main__" :
