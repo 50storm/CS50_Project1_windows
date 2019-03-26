7e21c24e6146e95dc4d379830c8f92ca44c3284f
50Storm <hirotech.net@gmail.com>
2019-03-26 21:27:29 +0900

    save

--- setting_mac.sh
+++ setting_mac.sh
@@ -4,8 +4,8 @@ echo "--settiing for--flask development [application.py]---"
 export FLASK_APP=application.py
 export FLASK_DEBUG=1
 #export FLASK_DEBUG=0
-#export DATABASE_URL=postgres://clbljlcvobhpyp:56202fd3f8b91471a55b86a150995293b195305c2a2cbf5ec0fc1e0314e1022c@ec2-107-22-221-60.compute-1.amazonaws.com:5432/ddrle3028kagbu
-export DATABASE_URL=postgres://hiroshi:Tera54hiro@localhost/bookreview
+export DATABASE_URL=postgres://clbljlcvobhpyp:56202fd3f8b91471a55b86a150995293b195305c2a2cbf5ec0fc1e0314e1022c@ec2-107-22-221-60.compute-1.amazonaws.com:5432/ddrle3028kagbu
+#export DATABASE_URL=postgres://hiroshi:Tera54hiro@localhost/bookreview
 
 echo "---printenv---"
 echo "environemt valus that flask requires"
